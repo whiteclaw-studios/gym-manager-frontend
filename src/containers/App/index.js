17e6192ffa9d6c25eaf4bd126662e87f5e6d698d
rajeshk <rajeshkumaranraj@gmail.com>
2020-11-04 20:55:52 +0530

    A2HS added

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -18,11 +18,40 @@ class App extends React.Component {
   constructor(props) {
     super(props);
   }
+  componentDidMount() {
+    let deferredPrompt = null;
+    const addBtn = document.querySelector('.add-button');
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI to notify the user they can add to home screen
+      addBtn.style.display = 'block';
 
+      addBtn.addEventListener('click', (e) => {
+        // hide our user interface that shows our A2HS button
+        // addBtn.style.display = 'none';
+        console.log('e fired', e);
+        // Show the prompt
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt
+        deferredPrompt.userChoice.then((choiceResult) => {
+          if (choiceResult.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt');
+          } else {
+            console.log('User dismissed the A2HS prompt');
+          }
+          deferredPrompt = null;
+        });
+      });
+    });
+  }
   render() {
     const { route = {} } = this.props;
     return (
       <Wrap>
+        <button className="add-button">Add to home screen</button>
         {renderRoutes(route.routes, {
           ...this.props,
         })}
