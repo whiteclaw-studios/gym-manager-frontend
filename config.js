67447f672736c2e2319b883bdea2c46576d3fb70
whiteclaw-studios <whiteclawstudios@gmail.com>
2020-11-05 01:59:32 +0530

    port changed

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const env = process.env;
 export const nodeEnv = env.NODE_ENV || 'development';
 export default {
-  port: env.PORT || 3000,
+  port: env.PORT || 80,
   host: env.HOST || '0.0.0.0',
   get serverUrl() {
     return `http://${this.host}:${this.port}`;
