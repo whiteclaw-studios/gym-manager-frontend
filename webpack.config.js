9067c78f5e3f55e2a4fb3a52b8091f61f51fc9c2
rajeshk <rajeshkumaranraj@gmail.com>
2020-11-20 19:32:04 +0530

    brotli plugin added

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { ReactLoadablePlugin } = require('react-loadable/webpack');
-
+const BrotliPlugin = require('brotli-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 module.exports = {
